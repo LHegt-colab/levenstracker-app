a43fb0e9ec2fab3576dc95797c972d6d5c245e82
LHegt-colab <louis.hegt2@gmail.com>
2026-01-20 10:23:30 +0100

    Fix: Update ThemeContext to use AppContextSupabase

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect } from 'react';
-import { useApp } from './AppContext';
+import { useApp } from './AppContextSupabase';
 
 const ThemeContext = createContext();
 
